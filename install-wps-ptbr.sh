92da3de74239e5e770894dd02ffe45c2b71b825d
Felipe Silva Peterle <felipe.peterle@CPS-DTP-1154.cpaps.local>
2023-12-21 09:22:13 -0300

    Alterando script language

--- install-wps-ptbr.sh
+++ install-wps-ptbr.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Define a URL de download do WPS Office e o arquivo de idioma
+# Defina as URLs dos arquivos necessários
 WPS_OFFICE_URL="https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/11711/wps-office_11.1.0.11711.XA_amd64.deb"
-WPS_LANGUAGE_PACKAGE="wps-language-pt-br-1.0.deb"
+WPS_LANGUAGE_URL="https://raw.githubusercontent.com/Felipe-Peterle/WPS-Office/main/wps-language-pt-br-1.0.deb"
+INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/Felipe-Peterle/WPS-Office/main/install.sh"
 
-# Baixa o pacote WPS Office para o diretório /tmp
-wget "$WPS_OFFICE_URL" -O /tmp/wps-office.deb
-
-# Instala o pacote WPS Office
+# Baixe e instale o pacote WPS Office
+wget -q "$WPS_OFFICE_URL" -O /tmp/wps-office.deb
 sudo dpkg -i /tmp/wps-office.deb
 
-# Instala o pacote de idioma
-sudo dpkg -i "$WPS_LANGUAGE_PACKAGE"
+# Baixe e instale o pacote de idioma português brasileiro
+wget -q "$WPS_LANGUAGE_URL" -O /tmp/wps-language-pt-br.deb
+sudo dpkg -i /tmp/wps-language-pt-br.deb
 
-# Executa o script de instalação adicional
-./install.sh
+# Baixe e execute o script de instalação adicional
+wget -q "$INSTALL_SCRIPT_URL" -O /tmp/install.sh
+sudo bash /tmp/install.sh
